d5ab1edf57b5c8aea3dc6c51e2f36ddfc20869b4
Sven Leach <info@svenno.de>
2025-12-09 13:43:48 +0100

    [SUGGESTION] Take artefacts into account
    I suggest muscle_bursts() to take artefactual data into account, such that artefactual data is set to nan when computing dynamic thresholds. This allows the function to work on any EEG data prompted to the function, not only EEG data where artefacts were set to 0 in a previous step. Also, I'm not sure about the enforced average referencing. I don't understand why this step cannot, like all the other, be performed on original referencing.

--- +sprep/+detect/muscle_bursts.m
+++ +sprep/+detect/muscle_bursts.m
@@ -1,20 +1,35 @@
-function Artefacts = muscle_bursts(EEG, RangeMuscle, MedianMultiplierThresholds, SmoothWindow)
+function [Artefacts, SmoothData, Thresholds] = muscle_bursts(EEG, RangeMuscle, MedianMultiplierThresholds, SmoothWindow, Artefacts)
 arguments
     EEG
     RangeMuscle = [20 45];
     MedianMultiplierThresholds = [20 100]; % x times the median for a burst threshold
     SmoothWindow = 0.2; % seconds
+    Artefacts = logical(zeros(size(EEG.data)))
 end
 
 disp('Detecting muscle artefacts')
 
-EEG = pop_reref(EEG, []);
+% EEG = pop_reref(EEG, []);
 
 % get data into bands
 MuscleEEG = sprep.eeg.timeband(EEG, RangeMuscle);
 
 SmoothData = sprep.eeg.smooth(MuscleEEG, SmoothWindow, 'mean');
 
-Thresholds = MedianMultiplierThresholds.*median(SmoothData.data, 'all');
+ThresholdData = SmoothData.data;
+ThresholdData(Artefacts) = nan;
+
+Thresholds = MedianMultiplierThresholds.*median(ThresholdData, 'all', 'omitmissing');
 
 Artefacts = sprep.utils.double_threshold(SmoothData.data, Thresholds(1), Thresholds(2));
+
+% %%
+% figure('Position', [50 50 500 250], 'color', 'w');
+% hold on;
+% plotme = SmoothData.data(1,:);
+% plot(plotme)
+% yline(Thresholds)
+% ylim([0 max(Thresholds)*1.2])
+% plotme(~Artefacts(1,:)) = nan;
+% plot(plotme, 'r')
+% xlim([0, 200*30])
