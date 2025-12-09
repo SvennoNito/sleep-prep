9454c31ee1409da94f16ff1f334624e99219f089
Sven Leach <info@svenno.de>
2025-12-09 13:31:22 +0100

    Removed hard coded sleep stages

--- +sprep/+scoring/detect_sleep_cycles.m
+++ +sprep/+scoring/detect_sleep_cycles.m
@@ -1,18 +1,29 @@
-function [Starts, Ends] = detect_sleep_cycles(Scoring, EpochLength, MinTime)
+function [Starts, Ends] = detect_sleep_cycles(Scoring, EpochLength, MinTime, opts)
 arguments
     Scoring
     EpochLength
     MinTime = 10; % minutes
+    opts.stagemap = dictionary( ...
+        'W', 1, ...
+        'REM', 0, ...
+        'N1', -1, ...
+        'N2', -2, ...
+        'N3', -3 ...
+        );
 end
 
 % ignore n1 and wake by assigning them to the previous stage
-Scoring(1) = -2; % assign first score to NREM sleep so that the next code can reassign wake and n1 appropriately
-Scoring = sprep.scoring.reassign_n1(Scoring, -1);
-Scoring = sprep.scoring.reassign_n1(Scoring, 0);
+Scoring(1) = opts.stagemap('N2'); % assign first score to NREM sleep so that the next code can reassign wake and n1 appropriately
+Scoring = sprep.scoring.reassign_n1(Scoring, opts.stagemap('N2'));
+Scoring = sprep.scoring.reassign_n1(Scoring, opts.stagemap('W'));
 Scoring(Scoring<0) = -1;
 
 % NREM bouts
-[Starts, Ends] = sprep.utils.data2windows(Scoring<=0);
+NREM = ...
+    ismember(Scoring, opts.stagemap('N1')) | ...
+    ismember(Scoring, opts.stagemap('N2')) | ...
+    ismember(Scoring, opts.stagemap('N3'));
+[Starts, Ends] = sprep.utils.data2windows(NREM);
 
 Durations = (Ends+1-Starts)*EpochLength;
 
